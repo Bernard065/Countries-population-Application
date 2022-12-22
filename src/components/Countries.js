b151baa4b1899cfcd3788f29f985deacbfc3f0b4
Bernard065 <bebenibernard@gmail.com>
2022-12-22 21:58:12 +0300

    set steady values

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,5 +1,10 @@
 import { useState, useEffect } from "react";
 
 export default function Countries() {
+    const [countries, setCountries] = useState([])
+
+    useEffect(() => {
+        
+    })
     return <>Countries</>;
 }
