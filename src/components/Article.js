c1910ac37b07c71c9cab62dbc157fcd2778b12ab
Bernard065 <bebenibernard@gmail.com>
2022-12-22 23:12:13 +0300

    render country details

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,9 +1,19 @@
 import React from 'react'
 
-function Article() {
+function Article({flags, name, population, region, subregion}) {
   return (
-    <div>Article</div>
-  )
+    <>
+        <article>
+            <img src={flags.svg} alt="" />
+            <h2>{name.common}</h2>
+            <ul>
+                <li>Pouplation: {population.toLocaleString()}</li>
+                <li>Region: {region}</li>
+                <li>Subregion: {subregion}</li>
+            </ul>
+        </article>
+    </>
+  );
 }
 
 export default Article
