ba2e5ff82408274673cae2aabc9259d130565b4b
Bernard065 <bebenibernard@gmail.com>
2022-12-22 23:19:26 +0300

    style the articles

--- src/components/Article.js
+++ src/components/Article.js
@@ -5,8 +5,8 @@ function Article({flags, name, population, region, subregion}) {
     <>
         <article>
             <img src={flags.svg} alt="" />
-            <h2>{name.common}</h2>
-            <ul>
+            <h2 className='font-bold text-lg text-gray-900 mb-2'>{name.common}</h2>
+            <ul className='flex flex-col items-start justify-start gap-2'>
                 <li>Pouplation: {population.toLocaleString()}</li>
                 <li>Region: {region}</li>
                 <li>Subregion: {subregion}</li>
