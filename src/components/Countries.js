d1b86b7e2183eedb66ba239e3e8bda962cc5d2a3
Bernard065 <bebenibernard@gmail.com>
2022-12-23 01:38:51 +0300

    add search form

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -25,6 +25,11 @@ export default function Countries() {
             </h1>
           ) : (
             <section className="container mx-auto p-10">
+                <div>
+                    <form>
+                        <input type="text" name="search" id="search" placeholder="Search for a country by its name" required/>
+                    </form>
+                </div>
                 <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
                     {countries.map((country) => (
                         <Article key={country.name.common} {...country} />
