4aa8d6281ef83a4b3bad94838ccc7ba8abe839d4
Bernard065 <bebenibernard@gmail.com>
2022-12-22 21:10:32 +0300

    importing SingleCountry from components/SingleCountry

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Countries from "./components/Countries";
-
+import SingleCountry from "./components/SingleCountry";
 
 function App() {
   return (
