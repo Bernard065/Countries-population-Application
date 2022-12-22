c36ac7001e290a835f0eff429a0eb2cda33e3e0c
Bernard065 <bebenibernard@gmail.com>
2022-12-22 21:35:59 +0300

    render Countries component into App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import SingleCountry from "./components/SingleCountry";
 function App() {
   return (
     <BrowserRouter>
+      
       <Routes>
         <Route path="/" element={<Countries />}></Route>
         <Route path="/:name" element={<SingleCountry />}></Route>
