002ae0462528bb912d4baaa1d9ac18e11991e6fd
Bernard065 <bebenibernard@gmail.com>
2022-12-22 22:14:38 +0300

    fetch data from Api

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -4,7 +4,17 @@ export default function Countries() {
     const [countries, setCountries] = useState([])
 
     useEffect(() => {
-        
-    })
-    return <>Countries</>;
+        const getCountries = async() => {
+            try {
+                const res = await fetch('https://restcountries.com/v3.1/all')
+                const data = await res.json()
+                setCountries(data)
+            }catch (error) {
+                console.error(error)
+            }
+        }
+
+        getCountries()
+    }, [])
+    return <>{countries.length}</>;
 }
